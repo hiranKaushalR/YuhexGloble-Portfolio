73d8c1fec6f93084ff97e5bd5d3557e20ca388f3
Hiran <134024847+hiranKaushalR@users.noreply.github.com>
2024-02-16 22:14:42 +0530

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,31 @@
+import css from "./css.png";
+import email from "./email.png";
+import facebook from "./facebook.png";
+import html from "./html.png";
+import instagram from "./instagram.png";
+import js from "./js.png";
+import linkedin from "./linkedin.png";
+import mysql from "./mysql.png";
+import nodejs from "./nodejs.png";
+import php from "./php.png";
+import react from "./react.png";
+import sass from "./sass.png";
+import telephone from "./telephone.png";
+import tiktok from "./tiktok.png";
+
+export {
+  css,
+  email,
+  facebook,
+  html,
+  instagram,
+  js,
+  linkedin,
+  mysql,
+  nodejs,
+  php,
+  react,
+  sass,
+  telephone,
+  tiktok,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,91 @@
+import {
+  css,
+  email,
+  facebook,
+  html,
+  instagram,
+  js,
+  linkedin,
+  mysql,
+  nodejs,
+  php,
+  react,
+  sass,
+  telephone,
+  tiktok,
+} from "../assets";
+
+export const tools = [
+  {
+    id: "html",
+    icon: html,
+  },
+  {
+    id: "css",
+    icon: css,
+  },
+  {
+    id: "sass",
+    icon: sass,
+  },
+  {
+    id: "js",
+    icon: js,
+  },
+  {
+    id: "php",
+    icon: php,
+  },
+  {
+    id: "mysql",
+    icon: mysql,
+  },
+  {
+    id: "react",
+    icon: react,
+  },
+  {
+    id: "html",
+    icon: html,
+  },
+  {
+    id: "nodejs",
+    icon: nodejs,
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "tiktok",
+    icon: tiktok,
+    link: "https://www.tiktok.com/",
+  },
+  {
+    id: "instagram",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "linkedin",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+  {
+    id: "facebook",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+];
+
+export const contact = [
+  {
+    id: "phone",
+    icon: telephone,
+    contact: "+94 75 706 0941",
+  },
+  {
+    id: "email",
+    icon: email,
+    contact: "info@yuhexglobal.com",
+  },
+];
